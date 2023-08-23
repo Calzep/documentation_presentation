ca9620eb9e473b03929c952a7f4d7fc99ba2da9e
Calzep <caleb-easpn@nmit.live.ac.nz>
2023-08-24 10:57:22 +1200

    Created reverse string solution

---
+++ solutions/caleb-eason/reverse-string.js
@@ -0,0 +1,10 @@
+/**
+ * Takes a string and returns it in reverse
+ *
+ * @param {string} stringInput - description
+ * @returns {string} description
+ */
+
+export function reverseString (stringInput){
+    return stringInput.split('').reverse().join('')
+}
